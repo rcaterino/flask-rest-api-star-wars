ca298285c3542e8a1fa122334d5f7d501c26fd40
Ricardo Fabio Caterino Polini <ricardocaterino@gmail.com>
2023-01-22 09:50:18 +0000

    ENDPOINT

--- src/main.py
+++ src/main.py
@@ -30,6 +30,159 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
+# get all people
+@app.route('/allpeople', methods=['GET'])
+def getAllPeople():
+    try:
+        people = People.query.all()
+        people_serialize = list(map(lambda x: x.serialize(), people))
+        return jsonify(people_serialize), 200
+    except:
+        raise APIException('No hay personajes en la BBDD', 404)
+
+
+# get one people
+@app.route('/allpeople/<int:people_id>')
+def getOnePeople(people_id):
+    try:
+        people = People.query.get(people_id)
+        return jsonify(people.serialize()), 200
+    except:
+        raise APIException('Personaje no encontrado', 404)
+
+
+# get all planets
+@app.route('/allplanets', methods=['GET'])
+def getAllPlanets():
+    try:
+        planet = Planet.query.all()
+        planet_serialize = list(map(lambda x: x.serialize(), planet))
+        return jsonify(planet_serialize), 200
+    except:
+        raise APIException('No hay planetas en la BBDD', 404)
+
+# get one planet
+@app.route('/allplanets/<int:planet_id>')
+def getOnePlanet():
+    try:
+        planet = Planet.query.get(planet_id)
+        return jsonify(planet.serialize())
+    except:
+        raise APIException('Planeta no encontrado', 404)
+
+# get all users
+@app.route('/allusers', methods=['GET'])
+def getAllUsers():
+    try:
+        users = User.query.all()
+        users_serialize = list(map(lambda x: x.serialize(), users))
+        return jsonify(people_serialize), 200
+    except:
+        raise APIException('No hay usuarios en la BBDD', 404)
+
+# get user favourites
+@app.route('/users/favorites/<int:user_id>', methods=['GET'])
+def getUserFavorites(user_id):
+    user = User.query.get(user_id)
+
+    if not user:
+        raise APIException('Usuario no existe', 404)
+
+    favourites = [fav.serialize() for fav in user.planet_favourites + user.people_favourites]
+
+    return jsonify(favourites), 200
+
+
+# Add favourite planet
+@app.route('/favourite/planet/<int:planet_id>', methods=['POST'])
+def addFavouritePlanet(planet_id):
+    #Asumimos que nos llega un body -> {"User": id}
+    resquest_body = request.json.get('User')
+
+    user = User.query.get(resquest_body)
+    if not user:
+        raise APIException('Usuario no existe', 404)
+
+    planet = Planet.query.get(planet_id)
+    if not planet:
+        raise APIException('Planeta no existe', 404)
+
+    if planet in user.planet_favourites:
+        raise APIException('Planeta ya está en favoritos', 404)
+
+    user.planet_favourites.append(planet)
+    db.session.commit()
+    return jsonify(f"Planeta {planet} añadido"), 200
+
+
+# add favourite character
+@app.route('/favourite/character/<int:people_id>', methods=['POST'])
+def addFavouriteCharacter(people_id):
+    #Asumimos que nos llega un body -> {"User": id}
+    resquest_body = request.json.get('User')
+
+    user = User.query.get(resquest_body)
+    if not user:
+        raise APIException('Usuario no existe', 404)
+
+    people = People.query.get(people_id)
+    if not people:
+        raise APIException('Personaje no existe', 404)
+
+    if people in user.people_favourites:
+        raise APIException('Personaje ya está en favoritos', 404)
+
+    user.people_favourites.append(people)
+    db.session.commit()
+    return jsonify(f"Personaje {people} añadido"), 200
+
+
+# delete favourite planet
+@app.route('/favourite/planet/<int:planet_id>', methods=['DELETE'])
+def deleteFavouritePlanet(planet_id):
+
+    #Asumimos que nos llega un body -> {"User": id}
+    resquest_body = request.json.get('User')
+
+    user = User.query.get(resquest_body)
+    if not user:
+        raise APIException('Usuario no existe', 404)
+
+    planet = Planet.query.get(planet_id)
+    if not planet:
+        raise APIException('Planeta no existe', 404)
+
+    if planet not in user.planet_favourites:
+        raise APIException('Planeta no existe en favoritos', 404)
+
+    user.planet_favourites.remove(planet)
+    db.session.commit()
+    return jsonify(f'El planeta {planet} ha sido eliminado de favoritos'), 200
+
+# delete favourite people
+@app.route('/favourite/planet/<int:people_id>', methods=['DELETE'])
+def deleteFavouritePeople(people_id):
+
+    #Asumimos que nos llega un body -> {"User": id}
+    resquest_body = request.json.get('User')
+
+    user = User.query.get(resquest_body)
+    if not user:
+        raise APIException('Usuario no existe', 404)
+
+    people = People.query.get(people_id)
+    if not people:
+        raise APIException('Personaje no existe', 404)
+
+    if people not in user.people_favourites:
+        raise APIException('El personaje no existe en favoritos', 404)
+
+    user.people_favourites.remove(people)
+    db.session.commit()
+    return jsonify(f'El personaje {people} ha sido eliminado de favoritos'), 200
+
+
 @app.route('/user', methods=['GET'])
 def handle_hello():
 
--- src/models.py
+++ src/models.py
@@ -17,3 +17,56 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class People(db.Model):
+    characterID = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(80), unique = True, nullable = False)
+    eyed_color = db.Column(db.String(80), unique = False, nullable = False)
+    birth_year = db.Column(db.String(80), unique = False, nullable = False)
+    height = db.Column(db.Integer, unique = False, nullable = False)
+    mass = db.Column(db.Integer, unique = False, nullable = False)
+    url = db.Column(db.String(200), unique = False, nullable = False)
+    homeworld = db.Column(db.String(200), unique = False, nullable = False)
+
+
+    def serialize(self):
+        return {
+            'characterID' : self.CharacterID,
+            'name' : self.name,
+            'eyed_color' : self.eyed_color,
+            'birth_year' : self.birth_year,
+            'height' : self.height,
+            'mass' : self.mass,
+            'url' : self.url,
+            'homeworld' : self.homeworld
+        }    
+
+class Planet(db.Model):
+    planetID = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(80), unique = True, nullable = False)
+    rotated_period  = db.Column(db.String(80), unique = False, nullable = False)
+    diameter = db.Column(db.Integer, unique = False, nullable = False)
+    climate = db.Column(db.String(80), unique = False, nullable = False)
+    orbital_period = db.Column(db.Integer, unique = False, nullable = False)
+    url = db.Column(db.String(200), unique = False, nullable = False)
+    
+    def serialize(self):
+        return {
+            'planetID' : self.planetID,
+            'name': self.name,
+            'rotated_period' : self.rotated_period,
+            'diameter': self.diameter,
+            'climate': self.climate,
+            'orbital_period': self.orbital_period,
+            'url': self.url
+        }   
+    
+planet_favourites = db.Table('planet_favourites',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('planet_id', db.Integer, db.ForeignKey('planet.planetID'), primary_key=True)
+)
+
+people_favourites = db.Table('people_favourites',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), nullable=False, primary_key=True),
+    db.Column('people_id', db.Integer, db.ForeignKey('people.characterID'), nullable=False, primary_key=True)
+)
